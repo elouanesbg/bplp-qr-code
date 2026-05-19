bf368310acb2f45cbebdc452dc98e1279950ef9d
elouanesbg <elouanesbg@gmail.com>
2026-05-19 10:43:13 +0100

    fix

--- src/pages/book.vue
+++ src/pages/book.vue
@@ -2,19 +2,26 @@
     <v-container v-if="book">
         <v-row>
             <v-col cols="12" md="4">
-                <v-img :src="book.cover.url" height="400" cover />
+                <v-img v-if="book.cover" :src="book.cover.url" height="400" cover />
+                <v-img v-else src="@/assets/bookcover_cover.png" height="400" cover />
+                <p v-if="book.isbn" class="mt-2 text-center">
+                    <b>ISBN:</b> {{ book.isbn }}
+                </p>
             </v-col>
 
             <v-col cols="12" md="8">
                 <h1>{{ book.title }}</h1>
-                <p><b>Author:</b> {{ book.author }}</p>
-                <p><b>Publisher:</b> {{ book.publisher }}</p>
-                <p><b>Year:</b> {{ book.publish_year }}</p>
-                <p><b>Pages:</b> {{ book.page_count }}</p>
+                <h3>{{ book.title_secondary }}</h3>
+                <p><b>الكاتب:</b> {{ book.author }}</p>
+                <p><b>الناشر:</b> {{ book.publisher }}</p>
+                <p><b>سنة النشر:</b> {{ book.publish_year }}</p>
+                <p><b>مكان النشر:</b> {{ book.publish_country }}</p>
+                <p><b>الطبعة:</b> {{ book.edition }}</p>
 
                 <p class="mt-4">{{ book.description }}</p>
 
-                <v-chip v-for="k in book.keywords" :key="k" class="ma-1" small>
+                <v-chip v-for="k in book.keywords.split('،').map(x => x.trim()).filter(Boolean)" :key="k" class="ma-1"
+                    small>
                     {{ k }}
                 </v-chip>
             </v-col>
